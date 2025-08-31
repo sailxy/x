386dad7d4311da74620267c9dd3cc083f0337b45
y <459419708@qq.com>
2025-08-31 19:42:32 +0800

    feat: get access token

--- wechat/open_platform.go
+++ wechat/open_platform.go
@@ -1,27 +1,54 @@
 package wechat
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/url"
+
+	"github.com/sailxy/x/rest"
 )
 
 type Config struct {
-	AppID string
+	AppID     string
+	AppSecret string
 }
 
 type WechatOpenPlatform struct {
-	appID string
+	appID     string
+	appSecret string
+
+	client *rest.REST
+}
+
+type errResp struct {
+	ErrCode int    `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+	RID     string `json:"rid"`
+}
+
+type getAccessTokenResp struct {
+	AccessToken  string `json:"access_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	OpenID       string `json:"openid"`
+	Scope        string `json:"scope"`
+	UnionID      string `json:"unionid"`
 }
 
 const openPlatformQRConnectURL = "https://open.weixin.qq.com/connect/qrconnect"
+const openPlatformGetAccessTokenURL = "https://api.weixin.qq.com/sns/oauth2/access_token"
 
 // NewWechatOpenPlatform create a new wechat open platform instance.
 func NewWechatOpenPlatform(c Config) *WechatOpenPlatform {
 	return &WechatOpenPlatform{
-		appID: c.AppID,
+		appID:     c.AppID,
+		appSecret: c.AppSecret,
+		client:    rest.NewREST(),
 	}
 }
 
 // QRConnect generate a QR connect URL for the wechat open platform.
+// After the user allows authorization, they will be redirected to the redirect_uri URL with code and state parameters.
 func (w *WechatOpenPlatform) QRConnect(redirectURI string, state string) (string, error) {
 	u, err := url.Parse(openPlatformQRConnectURL)
 	if err != nil {
@@ -37,4 +64,42 @@ func (w *WechatOpenPlatform) QRConnect(redirectURI string, state string) (string
 
 	u.RawQuery = q.Encode()
 	return u.String(), nil
+}
+
+// GetAccessToken get access_token through code.
+func (w *WechatOpenPlatform) GetAccessToken(code string) (*getAccessTokenResp, error) {
+	u, err := url.Parse(openPlatformGetAccessTokenURL)
+	if err != nil {
+		return nil, err
+	}
+
+	q := url.Values{}
+	q.Set("appid", w.appID)
+	q.Set("secret", w.appSecret)
+	q.Set("code", code)
+	q.Set("grant_type", "authorization_code")
+
+	u.RawQuery = q.Encode()
+	resp, err := w.client.Get(u.String())
+	if err != nil {
+		return nil, err
+	}
+
+	// Check error first.
+	var errResp errResp
+	err = json.Unmarshal(resp.Bytes(), &errResp)
+	if err != nil {
+		return nil, err
+	}
+	if errResp.ErrCode != 0 {
+		return nil, fmt.Errorf("errcode: %d, errmsg: %s, rid: %s", errResp.ErrCode, errResp.ErrMsg, errResp.RID)
+	}
+
+	// Parse response.
+	var respData getAccessTokenResp
+	err = json.Unmarshal(resp.Bytes(), &respData)
+	if err != nil {
+		return nil, err
+	}
+	return &respData, nil
 }
--- wechat/open_platform_test.go
+++ wechat/open_platform_test.go
@@ -1,14 +1,21 @@
 package wechat
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func newWechatOpenPlatform() *WechatOpenPlatform {
+	return NewWechatOpenPlatform(Config{
+		AppID:     os.Getenv("WECHAT_OPEN_WEB_APP_ID"),
+		AppSecret: os.Getenv("WECHAT_OPEN_WEB_APP_SECRET"),
+	})
+}
+
 func TestQRConnect(t *testing.T) {
-	c := Config{AppID: "appid_123"}
-	w := NewWechatOpenPlatform(c)
+	w := newWechatOpenPlatform()
 
 	tests := []struct {
 		name        string
@@ -23,6 +30,30 @@ func TestQRConnect(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := w.QRConnect(tt.redirectURI, tt.state)
 			if assert.NoError(t, err) {
+				t.Log(got)
+			}
+		})
+	}
+}
+
+func TestGetAccessToken(t *testing.T) {
+	w := newWechatOpenPlatform()
+
+	tests := []struct {
+		name    string
+		code    string
+		wantErr bool
+	}{
+		{"basic", "code123", false},
+		{"invalid_code", "invalid_code", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := w.GetAccessToken(tt.code)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else if assert.NoError(t, err) {
 				t.Log(got)
 			}
 		})
