49d2dbb87432ed8e3bfa76ca881e17b537e40db2
y <459419708@qq.com>
2026-03-22 03:55:50 +0800

    feat: generate upload path

--- util/fsutil/fsutil.go
+++ util/fsutil/fsutil.go
@@ -1,8 +1,14 @@
 package fsutil
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"os"
+	"path"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gookit/goutil/fsutil"
 )
@@ -10,6 +16,27 @@ import (
 // CreateFile creates a file and automatically creates a directory if the file directory does not exist.
 func CreateFile(path string) (*os.File, error) {
 	return fsutil.CreateFile(path, 0644, 0755)
+}
+
+// GenerateUploadPath builds an object key for uploaded files.
+func GenerateUploadPath(userID int64, filename string) string {
+	const defaultPrefix = "uploads"
+	const emptyName = "file"
+
+	filename = normalizeUploadFilename(filename)
+	ext := sanitizeUploadExtension(path.Ext(filename))
+	name := strings.TrimSuffix(filename, path.Ext(filename))
+	name = sanitizeUploadName(name)
+	if name == "" {
+		name = emptyName
+	}
+
+	keyName := time.Now().Format("20060102150405") + "-" + randomUploadSuffix() + "-" + name
+	if ext != "" {
+		keyName += "." + ext
+	}
+
+	return path.Join(defaultPrefix, strconv.FormatInt(userID, 10), keyName)
 }
 
 // https://github.com/golang/go/blob/9e3b1d53a012e98cfd02de2de8b1bd53522464d4/src/cmd/go/internal/modload/init.go#L1504C1-L1522C2
@@ -31,4 +58,67 @@ func FindModuleRoot(dir string) string {
 		dir = d
 	}
 	return ""
+}
+
+func normalizeUploadFilename(filename string) string {
+	filename = strings.ReplaceAll(filename, "\\", "/")
+	filename = path.Base(filename)
+	if filename == "." || filename == "/" {
+		return ""
+	}
+	return filename
+}
+
+func sanitizeUploadName(name string) string {
+	var b strings.Builder
+	lastDash := false
+
+	for _, r := range name {
+		if isASCIIAlphaNumeric(r) {
+			if 'A' <= r && r <= 'Z' {
+				r = r + ('a' - 'A')
+			}
+			b.WriteRune(r)
+			lastDash = false
+			continue
+		}
+
+		if !lastDash && b.Len() > 0 {
+			b.WriteByte('-')
+			lastDash = true
+		}
+	}
+
+	return strings.Trim(b.String(), "-")
+}
+
+func sanitizeUploadExtension(ext string) string {
+	ext = strings.TrimPrefix(strings.ToLower(ext), ".")
+	if ext == "" {
+		return ""
+	}
+
+	var b strings.Builder
+	for _, r := range ext {
+		if isASCIIAlphaNumeric(r) {
+			if 'A' <= r && r <= 'Z' {
+				r = r + ('a' - 'A')
+			}
+			b.WriteRune(r)
+		}
+	}
+
+	return b.String()
+}
+
+func isASCIIAlphaNumeric(r rune) bool {
+	return ('a' <= r && r <= 'z') || ('A' <= r && r <= 'Z') || ('0' <= r && r <= '9')
+}
+
+func randomUploadSuffix() string {
+	var buf [2]byte
+	if _, err := rand.Read(buf[:]); err != nil {
+		return "0000"
+	}
+	return hex.EncodeToString(buf[:])
 }
--- util/fsutil/fsutil_test.go
+++ util/fsutil/fsutil_test.go
@@ -2,6 +2,8 @@ package fsutil
 
 import (
 	"os"
+	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,4 +23,81 @@ func TestFindModuleRoot(t *testing.T) {
 
 	roots := FindModuleRoot(dir)
 	assert.NotEmpty(t, roots)
+}
+
+func TestGenerateUploadKey(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		userID   int64
+		filename string
+		pattern  string
+	}{
+		{
+			name:     "normal filename",
+			userID:   123,
+			filename: "abc.png",
+			pattern:  `^uploads/123/\d{14}-[0-9a-f]{4}-abc\.png$`,
+		},
+		{
+			name:     "symbols become dashes",
+			userID:   123,
+			filename: "a_b c@d.png",
+			pattern:  `^uploads/123/\d{14}-[0-9a-f]{4}-a-b-c-d\.png$`,
+		},
+		{
+			name:     "empty stem falls back to file",
+			userID:   123,
+			filename: "___.PNG",
+			pattern:  `^uploads/123/\d{14}-[0-9a-f]{4}-file\.png$`,
+		},
+		{
+			name:     "repeated junk collapses",
+			userID:   123,
+			filename: "a---___***b.png",
+			pattern:  `^uploads/123/\d{14}-[0-9a-f]{4}-a-b\.png$`,
+		},
+		{
+			name:     "no extension",
+			userID:   456,
+			filename: "report",
+			pattern:  `^uploads/456/\d{14}-[0-9a-f]{4}-report$`,
+		},
+		{
+			name:     "drops path parts",
+			userID:   789,
+			filename: `foo/bar\\baz?.PDF`,
+			pattern:  `^uploads/789/\d{14}-[0-9a-f]{4}-baz\.pdf$`,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			key := GenerateUploadPath(tt.userID, tt.filename)
+			assert.Regexp(t, regexp.MustCompile(tt.pattern), key)
+		})
+	}
+}
+
+func TestGenerateUploadKeyRandomSuffix(t *testing.T) {
+	t.Parallel()
+
+	keys := make(map[string]struct{})
+	for range 20 {
+		key := GenerateUploadPath(123, "abc.png")
+		parts := strings.Split(key, "/")
+		if assert.Len(t, parts, 3) {
+			nameParts := strings.Split(parts[2], "-")
+			if assert.GreaterOrEqual(t, len(nameParts), 3) {
+				assert.Regexp(t, regexp.MustCompile(`^[0-9a-f]{4}$`), nameParts[1])
+			}
+		}
+		keys[key] = struct{}{}
+	}
+
+	assert.Greater(t, len(keys), 1)
 }
